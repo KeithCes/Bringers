c37d4c2405eab8fafec095552c183f42ca8384ea
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2021-12-19 12:56:28 -0500

    Built out tabbar with background colors

--- Bringers/ContentView.swift
+++ Bringers/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = "ass"
+    
+    init() {
+        UITabBar.appearance().barTintColor = UIColor(CustomColors.tabbarGray)
+        UITabBar.appearance().alpha = 0.5
+        UITabBar.appearance().unselectedItemTintColor = UIColor(CustomColors.darkGray).withAlphaComponent(0.5)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(CustomColors.seafoamGreen)
+                .ignoresSafeArea()
+            
+            
+            Text("Bookmark Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Bookmark")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(CustomColors.seafoamGreen)
+                .ignoresSafeArea()
+            
+            Text("Video Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "video.circle.fill")
+                    Text("Video")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(CustomColors.seafoamGreen)
+                .ignoresSafeArea()
+        }
+        .accentColor(Color.black)
     }
 }
 
---
+++ Bringers/CustomColors.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColors.swift
+//  Bringers
+//
+//  Created by Keith C on 12/19/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomColors {
+    static let seafoamGreen = Color(hex: "A2EBE6")
+    static let darkGray = Color(hex: "686868")
+    static let tabbarGray = Color(hex: "D7D5D5")
+}
