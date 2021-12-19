03f407b450faa3dfa2a2341676d58181e8f805a2
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2021-12-18 22:15:48 -0500

    Initial Commit

---
+++ Bringers/BringersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BringersApp.swift
+//  Bringers
+//
+//  Created by Keith C on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BringersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bringers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bringers
+//
+//  Created by Keith C on 12/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
