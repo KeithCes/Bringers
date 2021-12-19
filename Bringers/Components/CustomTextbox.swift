dcffb815b999814a36dee72b9cfea6cc1bc4e3fb
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2021-12-19 16:34:41 -0500

    Added all textboxes (description not finished)

--- Bringers/Components/CustomTextbox.swift
+++ Bringers/Components/CustomTextbox.swift
@@ -11,23 +11,34 @@ import SwiftUI
 struct CustomTextbox: View {
 
     @Binding var field: String
+    var placeholderText: String
+    var height: CGFloat
+    var width: CGFloat
+    var charLimit: Int
 
-    init(field: Binding<String>) {
+    init(field: Binding<String>, placeholderText: String, height: CGFloat = 50, width: CGFloat = 322, charLimit: Int = 20) {
         self._field = field
+        self.placeholderText = placeholderText
+        self.height = height
+        self.width = width
+        self.charLimit = charLimit
     }
 
     var body: some View {
-        TextField(field, text: $field)
+        TextField(self.field, text: self.$field)
             .font(.system(size: 18, weight: .regular, design: .rounded))
             .foregroundColor(CustomColors.midGray)
             .fixedSize(horizontal: false, vertical: true)
             .multilineTextAlignment(.center)
             .background(Rectangle()
                             .fill(Color.white.opacity(0.5))
-                            .frame(width: 322, height: 50)
+                            .frame(width: self.width, height: self.height)
                             .cornerRadius(15))
-            .placeholder(when: field.isEmpty) {
-                Text("Pick-up or buy?").foregroundColor(CustomColors.midGray.opacity(0.6))
+            .placeholder(when: self.field.isEmpty) {
+                Text(self.placeholderText).foregroundColor(CustomColors.midGray.opacity(0.6))
+            }
+            .onReceive(self.field.publisher.collect()) {
+                self.field = String($0.prefix(self.charLimit))
             }
     }
 }
--- Bringers/ContentView.swift
+++ Bringers/ContentView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var name: String = ""
+    @State private var pickupBuy: String = ""
+    @State private var deliveryFee: String = ""
+    @State private var maxItemPrice: String = ""
+    @State private var itemName: String = ""
+    @State private var description: String = ""
     
     init() {
         UITabBar.appearance().barTintColor = UIColor(CustomColors.tabbarGray)
         UITabBar.appearance().alpha = 0.5
         UITabBar.appearance().unselectedItemTintColor = UIColor(CustomColors.darkGray).withAlphaComponent(0.5)
+        
+        UITextView.appearance().backgroundColor = .clear
     }
     
     var body: some View {
@@ -23,8 +29,29 @@ struct ContentView: View {
                 Text("LOOKING FOR SOMETHING?")
                     .font(.system(size: 48, weight: .bold, design: .rounded))
                 
-                CustomTextbox(field: $name)
-                    
+                CustomTextbox(field: $pickupBuy, placeholderText: "Pick-up or buy?")
+                    .padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))
+                
+                HStack {
+                    CustomTextbox(field: $deliveryFee, placeholderText: "Delivery Fee", width: 153)
+                    CustomTextbox(field: $maxItemPrice, placeholderText: "Max Item Price", width: 153)
+                }
+                .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+                
+                CustomTextbox(field: $itemName, placeholderText: "Name of Item")
+                    .padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))
+                
+                TextEditor(text: $description)
+                    .padding(EdgeInsets(top: 30, leading: 20, bottom: 20, trailing: 20))
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                    .background(Rectangle()
+                                    .fill(Color.white.opacity(0.5))
+                                    .frame(width: 322, height: 153)
+                                    .cornerRadius(15))
+                    .frame(minWidth: 0, maxWidth: 200, minHeight: 0, maxHeight: 200)
+                    .placeholder(when: self.description.isEmpty) {
+                        Text("Description").foregroundColor(CustomColors.midGray.opacity(0.6))
+                    }
             }
             .tabItem {
                 Image(systemName: "house.fill")
