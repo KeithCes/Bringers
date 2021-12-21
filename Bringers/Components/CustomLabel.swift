8e2c30616c063bf6050bbf745ec5d4ecf2ffeff2
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2021-12-21 00:27:02 -0500

    Added confirm view

---
+++ Bringers/Components/CustomLabel.swift
@@ -0,0 +1,40 @@
+//
+//  CustomLabel.swift
+//  Bringers
+//
+//  Created by Keith C on 12/20/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomLabel: View {
+    
+    var labelText: String
+    var height: CGFloat
+    var width: CGFloat
+    var isBold: Bool
+    var fontSize: CGFloat
+    var hasBackground: Bool
+    
+    init(labelText: String, height: CGFloat = 50, width: CGFloat = 322, isBold: Bool = false, fontSize: CGFloat = 18, hasBackground: Bool = true) {
+        self.labelText = labelText
+        self.height = height
+        self.width = width
+        self.isBold = isBold
+        self.fontSize = fontSize
+        self.hasBackground = hasBackground
+    }
+    
+    var body: some View {
+        Text(self.labelText)
+            .font(.system(size: self.fontSize, weight: self.isBold ? .bold : .regular, design: .rounded))
+            .foregroundColor(CustomColors.midGray)
+            .background(Rectangle()
+                            .fill(Color.white.opacity(0.5))
+                            .frame(width: self.width, height: self.height)
+                            .cornerRadius(15))
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(minWidth: 0, maxWidth: self.width-22, minHeight: 0, maxHeight: self.height-10)
+    }
+}
--- Bringers/Views/PlaceOrderView.swift
+++ Bringers/Views/PlaceOrderView.swift
@@ -16,66 +16,71 @@ struct PlaceOrderView: View {
     @State private var itemName: String = ""
     @State private var description: String = ""
     
+    @State private var isShowingConfirm = false
+    
     @ObservedObject private var keyboard = KeyboardResponder()
     
     var body: some View {
-    VStack {
-        Text("LOOKING FOR SOMETHING?")
-            .font(.system(size: 48, weight: .bold, design: .rounded))
-        
-        CustomTextbox(field: $pickupBuy, placeholderText: "Pick-up or buy?")
-            .padding(EdgeInsets(top: 30, leading: 20, bottom: 15, trailing: 20))
-        
-        HStack {
-            CustomTextbox(field: $deliveryFee, placeholderText: "Delivery Fee", width: 153)
-            CustomTextbox(field: $maxItemPrice, placeholderText: "Max Item Price", width: 153)
-        }
-        .padding(EdgeInsets(top: 20, leading: 20, bottom: 5, trailing: 20))
-        
-        CustomTextbox(field: $itemName, placeholderText: "Name of Item")
-            .padding(EdgeInsets(top: 30, leading: 20, bottom: 30, trailing: 20))
-        
-        TextEditor(text: $description)
-            .padding()
-            .font(.system(size: 18, weight: .regular, design: .rounded))
-            .foregroundColor(CustomColors.midGray)
+        VStack {
+            Text("LOOKING FOR SOMETHING?")
+                .font(.system(size: 48, weight: .bold, design: .rounded))
+            
+            CustomTextbox(field: $pickupBuy, placeholderText: "Pick-up or buy?")
+                .padding(EdgeInsets(top: 30, leading: 20, bottom: 15, trailing: 20))
+            
+            HStack {
+                CustomTextbox(field: $deliveryFee, placeholderText: "Delivery Fee", width: 153)
+                CustomTextbox(field: $maxItemPrice, placeholderText: "Max Item Price", width: 153)
+            }
+            .padding(EdgeInsets(top: 20, leading: 20, bottom: 5, trailing: 20))
+            
+            CustomTextbox(field: $itemName, placeholderText: "Name of Item")
+                .padding(EdgeInsets(top: 30, leading: 20, bottom: 30, trailing: 20))
+            
+            TextEditor(text: $description)
+                .padding()
+                .font(.system(size: 18, weight: .regular, design: .rounded))
+                .foregroundColor(CustomColors.midGray)
+                .background(Rectangle()
+                                .fill(Color.white.opacity(0.5))
+                                .frame(width: 322, height: 153)
+                                .cornerRadius(15))
+                .frame(minWidth: 0, maxWidth: 300, minHeight: 0, maxHeight: 140)
+                .placeholderTopLeft(when: self.description.isEmpty) {
+                    Text("Description").foregroundColor(CustomColors.midGray.opacity(0.6))
+                    // makes placeholder even with text in box, not sure why we need this padding
+                        .padding(.top, 24)
+                        .padding(.leading, 20)
+                }
+                .onReceive(self.description.publisher.collect()) {
+                    self.description = String($0.prefix(200))
+                }
+            
+            Button("PLACE ORDER") {
+                isShowingConfirm.toggle()
+            }
+            .popover(isPresented: $isShowingConfirm) {
+                ConfirmOrderView()
+            }
+            .padding(EdgeInsets(top: 35, leading: 20, bottom: 35, trailing: 20))
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .foregroundColor(Color.white)
             .background(Rectangle()
-                            .fill(Color.white.opacity(0.5))
-                            .frame(width: 322, height: 153)
+                            .fill(CustomColors.blueGray.opacity(0.6))
+                            .frame(width: 322, height: 70)
                             .cornerRadius(15))
-            .frame(minWidth: 0, maxWidth: 300, minHeight: 0, maxHeight: 140)
-            .placeholderTopLeft(when: self.description.isEmpty) {
-                Text("Description").foregroundColor(CustomColors.midGray.opacity(0.6))
-                // makes placeholder even with text in box, not sure why we need this padding
-                    .padding(.top, 24)
-                    .padding(.leading, 20)
-            }
-            .onReceive(self.description.publisher.collect()) {
-                self.description = String($0.prefix(200))
-            }
-        
-        Button("PLACE ORDER") {
-            print("Button tapped!")
+            
         }
-        .padding(EdgeInsets(top: 35, leading: 20, bottom: 35, trailing: 20))
-        .font(.system(size: 30, weight: .bold, design: .rounded))
-        .foregroundColor(Color.white)
-        .background(Rectangle()
-                        .fill(CustomColors.blueGray.opacity(0.6))
-                        .frame(width: 322, height: 70)
-                        .cornerRadius(15))
-        
+        .padding(.bottom, keyboard.currentHeight)
+        .edgesIgnoringSafeArea(.bottom)
+        .animation(.easeOut(duration: 0.16))
+        .tabItem {
+            Image(systemName: "house.fill")
+            Text("Home")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(CustomColors.seafoamGreen)
+        .ignoresSafeArea()
+        .gesture(DragGesture().onChanged{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)})
     }
-    .padding(.bottom, keyboard.currentHeight)
-    .edgesIgnoringSafeArea(.bottom)
-    .animation(.easeOut(duration: 0.16))
-    .tabItem {
-        Image(systemName: "house.fill")
-        Text("Home")
-    }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(CustomColors.seafoamGreen)
-    .ignoresSafeArea()
-    .gesture(DragGesture().onChanged{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)})
-}
 }
