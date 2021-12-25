861bb0a20dcb9d28d8d5bee5330cc3628a09123e
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2021-12-25 12:08:22 -0500

    Added animation to waiting screen

--- Bringers/Views/WaitingForBringerView.swift
+++ Bringers/Views/WaitingForBringerView.swift
@@ -14,12 +14,46 @@ struct WaitingForBringerView: View {
     
     @State private var isShowingOrderComing = false
     
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
         VStack {
             Text("WAITING FOR A BRINGER TO ACCEPT YOUR ORDER...")
                 .font(.system(size: 48, weight: .bold, design: .rounded))
                 .padding(EdgeInsets(top: 20, leading: 20, bottom: 15, trailing: 20))
                 .fixedSize(horizontal: false, vertical: true)
+            
+            Rectangle()
+                .frame(width: 200, height: 200)
+                .background(CustomColors.midGray.opacity(0.5))
+                .foregroundColor(CustomColors.midGray.opacity(0.5))
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(CustomColors.midGray.opacity(0.5))
+                        .scaleEffect(animationAmount)
+                        .opacity(Double(2 - animationAmount))
+                        .animation(
+                            .easeInOut(duration: 2)
+                                .repeatForever(autoreverses: false),
+                            value: animationAmount
+                        )
+                )
+                .overlay(
+                    Image(systemName: "person")
+                        .frame(width: 200, height: 200)
+                        .scaleEffect(animationAmount + 2)
+                        .opacity(Double(2 - animationAmount))
+                        .animation(
+                            .easeInOut(duration: 2)
+                                .repeatForever(autoreverses: false),
+                            value: animationAmount
+                        )
+                )
+                .onAppear {
+                    animationAmount = 2
+                }
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 40, trailing: 20))
             
             Button {
                 // TODO: confirmation screen/backend call to cancel order
