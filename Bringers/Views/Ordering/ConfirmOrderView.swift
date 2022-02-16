5f3643ee0a14d658e81c3fed6851a28f21855ed8
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2022-02-16 15:33:26 -0500

    Collect delvieryFee from credit card from orderers

--- Bringers/Views/Ordering/ConfirmOrderView.swift
+++ Bringers/Views/Ordering/ConfirmOrderView.swift
@@ -19,12 +19,14 @@ struct ConfirmOrderView: View {
     @Binding private var confirmPressed: Bool
     @Binding private var order: OrderModel
     
-    @State private var paymentMethodParams: STPPaymentMethodParams?
+    @State private var isShowingPaymentSheet: Bool = false
     
-    @State private var isActive: Bool = false
+    @State private var ordererInfo: UserInfoModel = UserInfoModel()
     
-    let paymentGatewayController = PaymentGatewayController()
+    @State private var paymentSheet: PaymentSheet?
+    @State private var paymentResult: PaymentSheetResult?
     
+    // TODO: i dont think this init is needed but im in the middle of something; delete and test later
     init(isShowingConfirm: Binding<Bool>, confirmPressed: Binding<Bool>, order: Binding<OrderModel>) {
         self._isShowingConfirm = isShowingConfirm
         self._confirmPressed = confirmPressed
@@ -33,63 +35,86 @@ struct ConfirmOrderView: View {
     
     var body: some View {
         VStack {
-            CustomTitleText(labelText: "CONFIRM THE FOLLOWING:")
-            
-            if order.pickupBuy == "Buy" {
-                CustomLabel(labelText: "ESTIMATED MAXIMUM COST:", isBold: true)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-                
-                // TODO: calc tax based on location (change 0.0625 to be dynamic)
-                let estTax = round(CGFloat(order.maxPrice) * 0.0625 * 100) / 100.0
-               
-                CustomLabel(labelText: "MAX ITEM PRICE = $" + String(format:"%.02f", order.maxPrice) + "\nDELIVERY FEE = $" + String(format:"%.02f", order.deliveryFee) + "\nESTIMATED SALES TAX = $" + "\(estTax)", height: 100)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
-                
-                CustomLabel(labelText: "TOTAL ESTIMATED MAXIMUM COST = $" + String(format:"%.02f", (estTax + order.maxPrice + order.deliveryFee)), height: 75, isBold: true)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-                
-                CustomLabel(labelText: "Note: Final costs may be slightly different than estimates due to actual item prices", height: 75, fontSize: 14)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 25, trailing: 20))
-            }
-            else {
-                CustomLabel(labelText: "DELIVERY FEE = $" + String(format:"%.02f", order.deliveryFee))
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
-                
-                CustomLabel(labelText: "TOTAL COST = $" + String(format:"%.02f", order.deliveryFee), isBold: true)
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-            }
-            
-            Text("By pressing “PLACE ORDER” you agree to the terms and conditions of the Bringers app")
-                .font(.system(size: 14, weight: .regular, design: .rounded))
-                .foregroundColor(CustomColors.midGray)
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(minWidth: 0, maxWidth: CustomDimensions.width, minHeight: 0, maxHeight: 10)
-                .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
-            
-            Button("PLACE ORDER") {
-                startCheckout { clientSecret in
-                
-                    PaymentConfig.shared.paymentIntentClientSecret = clientSecret
+            if isShowingPaymentSheet {
+                VStack {
+                    CustomTitleText(labelText: "CONFIRM PAYMENT METHOD")
                     
-                    DispatchQueue.main.async {
-                        isActive = true
+                    CustomLabel(labelText: "On confirmation of payment type you will be charged and the order will be placed", height: 75, fontSize: 14)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 25, trailing: 20))
+                    
+                    if let paymentSheet = self.paymentSheet {
+                        PaymentSheet.PaymentButton(
+                            paymentSheet: paymentSheet,
+                            onCompletion: self.onPaymentCompletion
+                        ) {
+                            Text("CONFIRM PAYMENT")
+                            .padding(EdgeInsets(top: 35, leading: 20, bottom: 35, trailing: 20))
+                            .font(.system(size: 30, weight: .bold, design: .rounded))
+                            .foregroundColor(Color.white)
+                            .background(Rectangle()
+                                            .fill(CustomColors.blueGray.opacity(0.6))
+                                            .frame(width: CustomDimensions.width, height: 70)
+                                            .cornerRadius(15))
+                        }
+                    } else {
+                        Text("Loading…")
                     }
                 }
             }
-            .fullScreenCover(isPresented: $isActive) {
-                CheckoutView()
+            else {
+                CustomTitleText(labelText: "CONFIRM THE FOLLOWING:")
+                
+                if order.pickupBuy == "Buy" {
+                    CustomLabel(labelText: "ESTIMATED MAXIMUM COST:", isBold: true)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                    
+                    // TODO: calc tax based on location (change 0.0625 to be dynamic)
+                    let estTax = round(CGFloat(order.maxPrice) * 0.0625 * 100) / 100.0
+                    
+                    CustomLabel(labelText: "MAX ITEM PRICE = $" + String(format:"%.02f", order.maxPrice) + "\nDELIVERY FEE = $" + String(format:"%.02f", order.deliveryFee) + "\nESTIMATED SALES TAX = $" + "\(estTax)", height: 100)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
+                    
+                    CustomLabel(labelText: "TOTAL ESTIMATED MAXIMUM COST = $" + String(format:"%.02f", (estTax + order.maxPrice + order.deliveryFee)), height: 75, isBold: true)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                    
+                    CustomLabel(labelText: "Note: Final costs may be slightly different than estimates due to actual item prices", height: 75, fontSize: 14)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 25, trailing: 20))
+                }
+                else {
+                    CustomLabel(labelText: "DELIVERY FEE = $" + String(format:"%.02f", order.deliveryFee))
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
+                    
+                    CustomLabel(labelText: "TOTAL COST = $" + String(format:"%.02f", order.deliveryFee), isBold: true)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                }
+                
+                Text("By pressing “PLACE ORDER” you agree to the terms and conditions of the Bringers app")
+                    .font(.system(size: 14, weight: .regular, design: .rounded))
+                    .foregroundColor(CustomColors.midGray)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(minWidth: 0, maxWidth: CustomDimensions.width, minHeight: 0, maxHeight: 10)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+                
+                Button("CONFIRM DETAILS") {
+                    preparePaymentSheet { _ in
+                        self.isShowingPaymentSheet.toggle()
+                    }
+                }
+                .padding(EdgeInsets(top: 35, leading: 20, bottom: 35, trailing: 20))
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .foregroundColor(Color.white)
+                .background(Rectangle()
+                                .fill(CustomColors.blueGray.opacity(0.6))
+                                .frame(width: CustomDimensions.width, height: 70)
+                                .cornerRadius(15))
             }
-            .padding(EdgeInsets(top: 35, leading: 20, bottom: 35, trailing: 20))
-            .font(.system(size: 30, weight: .bold, design: .rounded))
-            .foregroundColor(Color.white)
-            .background(Rectangle()
-                            .fill(CustomColors.blueGray.opacity(0.6))
-                            .frame(width: CustomDimensions.width, height: 70)
-                            .cornerRadius(15))
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(CustomColors.seafoamGreen)
         .ignoresSafeArea()
+        .onAppear {
+            self.getYourProfile()
+        }
     }
     
     func sendOrder() {
@@ -114,49 +139,100 @@ struct ConfirmOrderView: View {
         
         confirmPressed = true
         isShowingConfirm = false
-        
-        pay()
     }
     
-    private func startCheckout(completion: @escaping (String?) -> Void) {
-        let url = URL(string: "https://bringers-nodejs.vercel.app/create-payment-intent")!
+    func getYourProfile() {
+        
+        let ref = Database.database().reference()
+        
+        ref.child("users").child(self.order.userID).child("userInfo").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            guard let activeUserInfo = snapshot.value as? NSDictionary else {
+                return
+            }
+            
+            guard let activeUserInfoMap = UserInfo.from(activeUserInfo) else {
+                return
+            }
+            
+            let userInfo = UserInfoModel(
+                dateOfBirth: activeUserInfoMap.dateOfBirth,
+                dateOfCreation: activeUserInfoMap.dateOfCreation,
+                email: activeUserInfoMap.email,
+                firstName: activeUserInfoMap.firstName,
+                lastName: activeUserInfoMap.lastName,
+                ordersCompleted: activeUserInfoMap.ordersCompleted,
+                ordersPlaced: activeUserInfoMap.ordersPlaced,
+                phoneNumber: activeUserInfoMap.phoneNumber,
+                profilePictureURL: activeUserInfoMap.profilePictureURL,
+                rating: activeUserInfoMap.rating,
+                stripeAccountID: activeUserInfoMap.stripeAccountID,
+                stripeCustomerID: activeUserInfoMap.stripeCustomerID,
+                address: activeUserInfoMap.address,
+                state: activeUserInfoMap.state,
+                city: activeUserInfoMap.city,
+                country: activeUserInfoMap.country,
+                zipcode: activeUserInfoMap.zipcode
+            )
+            
+            self.ordererInfo = userInfo
+        })
+    }
+    
+    func preparePaymentSheet(completion: @escaping (String?) -> Void) {
+        let url = URL(string: "https://bringers-nodejs.vercel.app/payment-sheet")!
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try! JSONEncoder().encode(["poo" : "ass"])
+        request.httpBody = try! JSONEncoder().encode([
+            "customerID" : self.ordererInfo.stripeCustomerID,
+            "deliveryFee" : "\(Int(self.order.deliveryFee * 100))"
+        ])
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil,
-                    (response as? HTTPURLResponse)?.statusCode == 200 else {
-                completion(nil)
-                return
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data,
+                  error == nil,
+                  (response as? HTTPURLResponse)?.statusCode == 200,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
+                  let customerId = json["customer"] as? String,
+                  let customerEphemeralKeySecret = json["ephemeralKey"] as? String,
+                  let intentClientSecret = json["paymentIntent"] as? String,
+                  let publishableKey = json["publishableKey"] as? String else {
+                      completion(nil)
+                      return
+                  }
+            
+            STPAPIClient.shared.publishableKey = publishableKey
+            // MARK: Create a PaymentSheet instance
+            var configuration = PaymentSheet.Configuration()
+            configuration.merchantDisplayName = "Bringers"
+            configuration.customer = .init(id: customerId, ephemeralKeySecret: customerEphemeralKeySecret)
+            // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+            // methods that complete payment after a delay, like SEPA Debit and Sofort.
+            configuration.allowsDelayedPaymentMethods = true
+            
+            DispatchQueue.main.async {
+                self.paymentSheet = PaymentSheet(paymentIntentClientSecret: intentClientSecret, configuration: configuration)
             }
-            let checkoutIntentResponse = try? JSONDecoder().decode(CheckoutIntentResponse.self, from: data)
-            completion(checkoutIntentResponse?.clientSecret)
+            
+            completion(intentClientSecret)
         }.resume()
     }
     
-    private func pay() {
-        guard let clientSecret = PaymentConfig.shared.paymentIntentClientSecret else {
-            return
-        }
+    func onPaymentCompletion(result: PaymentSheetResult) {
+        self.paymentResult = result
         
-        let paymentIntentParams = STPPaymentIntentParams(clientSecret: clientSecret)
-        paymentIntentParams.paymentMethodParams = paymentMethodParams
-        
-        paymentGatewayController.submitPayment(intent: paymentIntentParams) { status, intent, error in
-            switch status {
-            case .failed:
-                print("failed")
-                break
-            case .canceled:
-                print("cancelled")
-                break
-            case .succeeded:
-                print("succeeded")
-                break
-            }
+        switch self.paymentResult {
+        case .completed:
+            sendOrder()
+            print("Payment complete")
+        case .failed(let error):
+            print("Payment failed: \(error.localizedDescription)")
+        case .canceled:
+            print("Payment canceled.")
+        case .none:
+            print("None?")
         }
     }
 }
